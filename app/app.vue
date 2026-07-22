b933742fdf0051d4439f0862a08b4ca83696ab43
AitHFifi <aithfifi@users.noreply.github.com>
2026-07-22 13:07:26 +0200

    feat: use transparent SVG favicon for crisp tab icon without white box

--- app/app.vue
+++ app/app.vue
@@ -26,6 +26,7 @@ const { isOpen: isContactOpen, close: closeContact } = useContactModal()
 
 useHead({
   link: [
+    { rel: 'icon', type: 'image/svg+xml', href: '/logo-wanda-classic-light.svg' },
     { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
     { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
     { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
